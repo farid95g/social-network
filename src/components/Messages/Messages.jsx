2d6544c79ec29173ba0c04e3496f9fde4b488152
farid95g <f95guluzade@gmail.com>
2021-05-22 23:55:35 +0400

    Messages component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,12 +1,63 @@
 import React from "react";
 import styles from "./Messages.module.css";
+import { NavLink } from "react-router-dom";
 
 const Messages = () => {
   return (
-    <section>
-      <h5>Messages</h5>
+    <section className={styles.messages}>
+      <div className={styles.messageSenders}>
+        <ul>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/1">
+              Albert Einstein
+            </NavLink>
+          </li>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/2">
+              Nikola Tesla
+            </NavLink>
+          </li>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/3">
+              Marie Curie
+            </NavLink>
+          </li>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/4">
+              Stephen Hawking
+            </NavLink>
+          </li>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/5">
+              Neil De Grass Tyson
+            </NavLink>
+          </li>
+          <li className={styles.sender + " " + styles.active}>
+            <NavLink activeClassName={styles.active} to="/messages/6">
+              Richard Feynman
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.messagesBlock}>
+        <div className={styles.message}>
+          <span>Hi</span>
+        </div>
+        <div className={styles.message}>
+          <span>How are you?</span>
+        </div>
+        <div className={styles.message}>
+          <span>Fine, thanks, and you?</span>
+        </div>
+        <div className={styles.message}>
+          <span>Ok</span>
+        </div>
+        <div className={styles.message}>
+          <span>No problem</span>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
 export default Messages;
