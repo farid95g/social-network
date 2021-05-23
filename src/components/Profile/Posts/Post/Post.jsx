8dc7221bb41d3b882340c4e4b8e30ea1a7af2852
farid95g <f95guluzade@gmail.com>
2021-05-23 19:06:00 +0400

    Post component changes and react-icons package

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./Post.module.css";
+import { FaThumbsUp } from "react-icons/fa";
 
 const Post = (props) => {
   return (
     <div className={styles.post}>
       <div>
         <img src="https://freesvg.org/img/Male-Avatar.png" alt="profile img" />
-        <span>{ props.message }</span>
+        <div>
+          <span>{ props.message }</span><br />
+          <span className={styles.like}>
+            <FaThumbsUp />
+            <span>{props.likesCount}</span>
+          </span>
+        </div>
       </div>
-      <span>likes { props.likesCount }</span>
     </div>
   );
 };
