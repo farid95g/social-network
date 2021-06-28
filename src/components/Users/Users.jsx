78a5519480602bdb1c5d51e65a1ae027c3f379c8
farid95g <f95guluzade@gmail.com>
2021-06-28 17:55:16 +0400

    users page pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,51 @@
 import * as axios from "axios";
 import styles from "./Users.module.css";
 import avatar from "./../../assets/images/avatar.jpg";
-import React from 'react';
+import React from "react";
 
 class Users extends React.Component {
   componentDidMount() {
     if (this.props.users.length === 0) {
       axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {
-        this.props.setUsers(response.data.items);
-      });
+        .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
     }
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+
+    axios
+        .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+  }
+
   render() {
+    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div>
+        <div className={styles.pagination}>
+          {
+            pages.map(p => {
+              return (
+                <button
+                  className={this.props.currentPage === p && styles.selectedPage}
+                  onClick={() => { this.onPageChanged(p); }}
+                >{p}</button>
+              )
+            })
+          }
+        </div>
         {this.props.users.map((u) => (
           <div key={u.id} className={styles.user}>
             <div>
@@ -27,7 +56,9 @@ class Users extends React.Component {
               />
               <br />
               {u.followed ? (
-                <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                <button onClick={() => this.props.unfollow(u.id)}>
+                  Unfollow
+                </button>
               ) : (
                 <button onClick={() => this.props.follow(u.id)}>Follow</button>
               )}
