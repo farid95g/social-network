e0f386f68ec85707955f21a75c26ffd1cbc43982
farid95g <f95guluzade@gmail.com>
2021-08-15 00:16:04 +0400

    follow and unfollow functionalities

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Users.module.css";
 import avatar from "./../../assets/images/avatar.jpg";
 import { NavLink } from "react-router-dom";
+import * as axios from "axios";
 
 const Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -39,10 +40,41 @@ const Users = (props) => {
               />
             </NavLink>
             <br />
-            {u.followed ? (
-              <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-            ) : (
-              <button onClick={() => props.follow(u.id)}>Follow</button>
+            {u.followed ?
+              (
+                <button onClick={
+                  () => {
+                    axios
+                      .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                          "API-KEY": "7af8cf62-29c6-4ae5-8d77-ed8358a66d04"
+                        }
+                      })
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
+                  }
+                }>Unfollow</button>
+              ) : (
+                <button onClick={
+                  () => {
+                    axios
+                      .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                        withCredentials: true,
+                        headers: {
+                          "API-KEY": "7af8cf62-29c6-4ae5-8d77-ed8358a66d04"
+                        }
+                      })
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
+                  }
+                }>Follow</button>
             )}
           </div>
           <div className={styles.userInfo}>
