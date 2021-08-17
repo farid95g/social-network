2011f73c32a297feae118d2dab65e7da8b89cfdd
farid95g <f95guluzade@gmail.com>
2021-08-17 21:42:43 +0400

    disable follow and unfollow button when action is in progress

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -43,8 +43,9 @@ const Users = (props) => {
             <br />
             {u.followed ?
               (
-                <button onClick={
+                <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={
                   () => {
+                    props.toggleFollowingProgress(true, u.id);
                     axios
                       .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
                         withCredentials: true,
@@ -56,17 +57,20 @@ const Users = (props) => {
                         if (response.data.resultCode === 0) {
                           props.unfollow(u.id);
                         }
+                        props.toggleFollowingProgress(false, u.id);
                       });
                   }
                 }>Unfollow</button>
               ) : (
-                <button onClick={
+                <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={
                   () => {
+                    props.toggleFollowingProgress(true, u.id);
                     followAPI.follow(u.id)
                       .then(data => {
                         if (data.resultCode === 0) {
                           props.follow(u.id);
                         }
+                        props.toggleFollowingProgress(true, u.id);
                       });
                   }
                 }>Follow</button>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as axios from "axios";
 import Users from "./Users";
 import { connect } from "react-redux";
-import { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching } from './../../redux/users-reducer';
+import { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress } from './../../redux/users-reducer';
 import Preloader from "../common/Preloader/Preloader";
 import { usersAPI } from "../../api/api";
 
@@ -38,6 +38,8 @@ class UsersContainer extends React.Component {
         users={this.props.users}
         follow={this.props.follow}
         unfollow={this.props.unfollow}
+        toggleFollowingProgress={this.props.toggleFollowingProgress}
+        followingInProgress={this.props.followingInProgress}
       />
     </>;
   }
@@ -49,10 +51,11 @@ const mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching
+    isFetching: state.usersPage.isFetching,
+    followingInProgress: state.usersPage.followingInProgress
   }
 }
 
 export default connect(mapStateToProps, {
-  follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching
+  follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress
 })(UsersContainer);
