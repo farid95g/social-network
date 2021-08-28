6712f5126ee9caf1962ab8c22ef03011863207b0
farid95g <f95guluzade@gmail.com>
2021-08-28 10:23:51 +0400

    profile status functionality

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -29,5 +29,5 @@ const mapStateToProps = (state) => ({
 export default compose(
   connect(mapStateToProps, { getUserProfile }),
   withRouter,
-  withAuthRedirect
+  // withAuthRedirect
 )(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import styles from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -16,6 +17,7 @@ const ProfileInfo = (props) => {
       <div>
         <img src={props.profile.photos.large} alt="profile img" />
       </div>
+      <ProfileStatus status="Hello my friends" />
     </div>
   );
 }
