af2d51dffd8d8c904e0a8a26c106a5095636bdd3
farid95g <f95guluzade@gmail.com>
2021-08-29 23:56:22 +0400

    toggleEditMode function

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,40 +1,39 @@
 import React from "react";
-import styles from "./ProfileInfo.module.css";
 
 class ProfileStatus extends React.Component {
   state = {
     editMode: false,
-    title: "Yo"
+    title: "Yo",
   };
 
-  activateEditMode() {
+  toggleEditMode = (isEditable) => {
     this.setState({
-      editMode: true
+      editMode: isEditable,
     });
-  }
-
-  deactivateEditMode() {
-    this.setState({
-      editMode: false
-    });
-  }
+  };
 
   render() {
     return (
       <div>
-        {!this.state.editMode &&
+        {!this.state.editMode && (
           <div>
-          <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+            <span onDoubleClick={() => this.toggleEditMode(true)}>
+              {this.props.status}
+            </span>
           </div>
-        }
-        {this.state.editMode &&
+        )}
+        {this.state.editMode && (
           <div>
-            <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
+            <input
+              autoFocus={true}
+              onBlur={() => this.toggleEditMode(false)}
+              value={this.props.status}
+            />
           </div>
-        }
+        )}
       </div>
     );
   }
-};
+}
 
 export default ProfileStatus;
